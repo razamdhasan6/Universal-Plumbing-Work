fe28effd756421547968f0e1999b5dc9cfb1108f
Md Hasan Raza <hasanrazabhai01@gmail.com>
2024-10-05 05:57:20 +0530

    first commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,21 +1,21 @@
 import { CiFacebook ,CiInstagram ,CiTwitter  } from "react-icons/ci";
 const Footer = () => {
   return (
-      <div className="bg-orange-100 p-8 flex flex-col gap-2 mt-10 text-center items-center justify-center">
+      <div className="bg-orange-100 p-8 flex flex-col gap-2  text-center items-center justify-center">
           <div className="flex gap-3 text-[2rem] cursor-pointer ">
               <span className="hover:scale-125 hover:bg-blue-200"><CiFacebook /></span>
               <span className="hover:scale-125 hover:bg-blue-200"><CiInstagram /></span>
               <span className="hover:scale-125 hover:bg-blue-200"><CiTwitter /></span>
       </div>
       <div className="flex gap-3 cursor-pointer">
-        <p className=" hover:bg-gray-200 hover:scale-125 hover:p-1 hover:rounded-lg">Home</p>
-        <p className=" hover:bg-gray-200 hover:scale-125 hover:p-1 hover:rounded-lg">Service's</p>
-        <p className=" hover:bg-gray-200 hover:scale-125 hover:p-1 hover:rounded-lg">Our Clints</p>
-        <p className=" hover:bg-gray-200 hover:scale-125 hover:p-1 hover:rounded-lg">Contact-Us</p>
-        <p className=" hover:bg-gray-200 hover:scale-125 hover:p-1 hover:rounded-lg">Book Now</p>
+        <p className=" hover:bg-gray-200 hover:scale-125 hover:p-1 hover:rounded-lg whitespace-nowrap text-[.85rem] md:text-[1rem]">Home</p>
+        <p className=" hover:bg-gray-200 hover:scale-125 hover:p-1 hover:rounded-lg whitespace-nowrap text-[.85rem] md:text-[1rem]">Service's</p>
+        <p className=" hover:bg-gray-200 hover:scale-125 hover:p-1 hover:rounded-lg whitespace-nowrap text-[.85rem] md:text-[1rem]">Our Clints</p>
+        <p className=" hover:bg-gray-200 hover:scale-125 hover:p-1 hover:rounded-lg whitespace-nowrap text-[.85rem] md:text-[1rem]">Contact-Us</p>
+        <p className=" hover:bg-gray-200 hover:scale-125 hover:p-1 hover:rounded-lg whitespace-nowrap text-[.85rem] md:text-[1rem]">Book Now</p>
       </div>
           <div>
-              <p>© 2024 A1 Plumbing Work</p>
+              <p className="text-[.7rem] md:text-[.9rem]">© 2024 A1 Plumbing Work</p>
           </div>
     </div>
   )
