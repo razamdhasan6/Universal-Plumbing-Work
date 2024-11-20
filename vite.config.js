bfae0ee1bf25ec2ec847021add8dd4c2dedb1f8f
Md Hasan Raza <hasanrazabhai01@gmail.com>
2024-11-20 19:52:00 +0530

    Your descriptive commit message

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Universal-Plumbing-Work/"
-
+  base: process.env.NODE_ENV === 'production' && process.env.VERCEL === undefined
+    ? '/Universal-Plumbing-Work/' // GitHub Pages base URL
+    : '/', // Default for Vercel and local environments
 })
